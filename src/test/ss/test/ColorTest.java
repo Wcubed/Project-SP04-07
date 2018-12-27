d1c803d9c46a3cd6a23fa72991ffca50396c1b59
Wybe <wybe@ruurdwestra.nl>
2018-12-27 12:43:09 +0100

    + Implemented color test.

--- src/test/ss/test/ColorTest.java
+++ src/test/ss/test/ColorTest.java
@@ -1,10 +1,26 @@
 package ss.test;
 
 import org.junit.jupiter.api.Test;
+import ss.spec.Color;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ColorTest {
 
     @Test
-    void isValidNextTo() {
+    void isValidNextToColors() {
+        assertTrue(Color.BLUE.isValidNextTo(Color.BLUE));
+        assertTrue(Color.RED.isValidNextTo(Color.RED));
+
+        assertFalse(Color.RED.isValidNextTo(Color.PURPLE));
+        assertFalse(Color.YELLOW.isValidNextTo(Color.GREEN));
+    }
+
+    @Test
+    void isValidNextToWhite() {
+        assertTrue(Color.BLUE.isValidNextTo(Color.WHITE));
+        assertTrue(Color.WHITE.isValidNextTo(Color.PURPLE));
+        assertTrue(Color.WHITE.isValidNextTo(Color.WHITE));
     }
 }
