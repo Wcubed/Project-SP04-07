ee352c05ac14d300f0e2eb06d19650974aa1e019
Wybe Westra <wybe@ruurdwestra.nl>
2019-01-25 13:32:47 +0100

    + Add basic game tests.

--- src/test/ss/test/gamepieces/MockTileBag.java
+++ src/test/ss/test/gamepieces/MockTileBag.java
@@ -1,5 +1,6 @@
 package ss.test.gamepieces;
 
+import ss.spec.gamepieces.Color;
 import ss.spec.gamepieces.EmptyTileBagException;
 import ss.spec.gamepieces.Tile;
 import ss.spec.gamepieces.TileBag;
@@ -23,11 +24,6 @@ public class MockTileBag implements TileBag {
     }
 
     @Override
-    public void addAllStartingTiles() {
-        // Do nothing.
-    }
-
-    @Override
     public Tile takeTile() throws EmptyTileBagException {
         if (getNumTilesLeft() == 0) {
             throw new EmptyTileBagException();
@@ -38,5 +34,72 @@ public class MockTileBag implements TileBag {
     @Override
     public int getNumTilesLeft() {
         return tiles.size();
+    }
+
+
+    /**
+     * Pre-determined order.
+     * Don't change this order, it is used to test the Game class' handling of the bag.
+     */
+    @Override
+    public void addAllStartingTiles() {
+
+        addTile(new Tile(Color.RED, Color.RED, Color.RED, 6));
+
+        addTile(new Tile(Color.RED, Color.YELLOW, Color.BLUE, 1));
+
+        // Two tiles of the same points, to see if the game can do tie brakes when
+        // determining the player order.
+        addTile(new Tile(Color.BLUE, Color.BLUE, Color.GREEN, 4));
+        addTile(new Tile(Color.BLUE, Color.BLUE, Color.YELLOW, 4));
+
+        addTile(new Tile(Color.YELLOW, Color.PURPLE, Color.RED, 2));
+
+        addTile(new Tile(Color.GREEN, Color.GREEN, Color.YELLOW, 4));
+        addTile(new Tile(Color.GREEN, Color.GREEN, Color.PURPLE, 4));
+
+        addTile(new Tile(Color.YELLOW, Color.PURPLE, Color.GREEN, 2));
+
+        addTile(new Tile(Color.RED, Color.RED, Color.YELLOW, 5));
+        addTile(new Tile(Color.RED, Color.RED, Color.PURPLE, 5));
+        addTile(new Tile(Color.BLUE, Color.BLUE, Color.RED, 5));
+
+        addTile(new Tile(Color.WHITE, Color.WHITE, Color.WHITE, 1));
+
+        addTile(new Tile(Color.YELLOW, Color.YELLOW, Color.BLUE, 5));
+        addTile(new Tile(Color.PURPLE, Color.PURPLE, Color.YELLOW, 5));
+        addTile(new Tile(Color.PURPLE, Color.PURPLE, Color.GREEN, 5));
+
+        addTile(new Tile(Color.BLUE, Color.BLUE, Color.BLUE, 6));
+        addTile(new Tile(Color.GREEN, Color.GREEN, Color.GREEN, 6));
+        addTile(new Tile(Color.YELLOW, Color.YELLOW, Color.YELLOW, 6));
+        addTile(new Tile(Color.PURPLE, Color.PURPLE, Color.PURPLE, 6));
+
+
+        addTile(new Tile(Color.RED, Color.RED, Color.BLUE, 4));
+        addTile(new Tile(Color.RED, Color.RED, Color.GREEN, 4));
+
+        addTile(new Tile(Color.YELLOW, Color.YELLOW, Color.RED, 4));
+        addTile(new Tile(Color.YELLOW, Color.YELLOW, Color.PURPLE, 4));
+
+        addTile(new Tile(Color.GREEN, Color.RED, Color.PURPLE, 1));
+        addTile(new Tile(Color.BLUE, Color.YELLOW, Color.GREEN, 1));
+
+        addTile(new Tile(Color.PURPLE, Color.PURPLE, Color.RED, 4));
+        addTile(new Tile(Color.PURPLE, Color.PURPLE, Color.BLUE, 4));
+
+        addTile(new Tile(Color.YELLOW, Color.BLUE, Color.PURPLE, 3));
+        addTile(new Tile(Color.RED, Color.GREEN, Color.YELLOW, 3));
+        addTile(new Tile(Color.BLUE, Color.GREEN, Color.PURPLE, 3));
+        addTile(new Tile(Color.GREEN, Color.RED, Color.BLUE, 3));
+
+        addTile(new Tile(Color.BLUE, Color.BLUE, Color.PURPLE, 5));
+        addTile(new Tile(Color.GREEN, Color.GREEN, Color.RED, 5));
+        addTile(new Tile(Color.GREEN, Color.GREEN, Color.BLUE, 5));
+        addTile(new Tile(Color.YELLOW, Color.YELLOW, Color.GREEN, 5));
+
+
+        addTile(new Tile(Color.BLUE, Color.RED, Color.PURPLE, 2));
+
     }
 }
--- src/test/ss/test/server/GameTest.java
+++ src/test/ss/test/server/GameTest.java
@@ -1,6 +1,133 @@
 package ss.test.server;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ss.spec.gamepieces.Board;
+import ss.spec.server.ClientPeer;
+import ss.spec.server.Game;
+import ss.test.gamepieces.MockTileBag;
+import ss.test.networking.MockConnection;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GameTest {
 
-    // TODO: Test the game class.
+    Game game;
+    Board board;
+    MockTileBag tileBag;
+
+    ClientPeer player1Alice;
+    MockConnection con1Alice;
+    ClientPeer player2Bob;
+    MockConnection con2Bob;
+    ClientPeer player3Clarice;
+    MockConnection con3Clarice;
+    ClientPeer player4Diane;
+    MockConnection con4Diane;
+
+    @BeforeEach
+    void setUp() {
+        con1Alice = new MockConnection();
+        con2Bob = new MockConnection();
+        con3Clarice = new MockConnection();
+        con4Diane = new MockConnection();
+
+        player1Alice = new ClientPeer(con1Alice);
+        player2Bob = new ClientPeer(con2Bob);
+        player3Clarice = new ClientPeer(con3Clarice);
+        player4Diane = new ClientPeer(con4Diane);
+
+        player1Alice.handleReceivedMessage("connect Alice");
+        player2Bob.handleReceivedMessage("connect Bob");
+        player3Clarice.handleReceivedMessage("connect Clarice");
+        player4Diane.handleReceivedMessage("connect Diane");
+
+        ArrayList<ClientPeer> players = new ArrayList<>();
+        players.add(player1Alice);
+        players.add(player2Bob);
+        players.add(player3Clarice);
+        players.add(player4Diane);
+
+        board = new Board();
+        tileBag = new MockTileBag();
+
+        game = new Game(players, board, tileBag);
+
+        game.setUpGame();
+    }
+
+    @Test
+    void startMessage() {
+        String messageAlice = con1Alice.readSentMessage();
+
+        System.out.println(messageAlice);
+
+        assertTrue(messageAlice.contains("start with"));
+        assertTrue(messageAlice.contains("Alice"));
+        assertTrue(messageAlice.contains("Bob"));
+        assertTrue(messageAlice.contains("Clarice"));
+        assertTrue(messageAlice.contains("Diane"));
+
+        // Bob should have gotten the same message.
+        String messageBob = con2Bob.readSentMessage();
+
+        System.out.println(messageBob);
+
+        assertTrue(messageBob.contains("start with"));
+        assertTrue(messageBob.contains("Alice"));
+        assertTrue(messageBob.contains("Bob"));
+        assertTrue(messageBob.contains("Clarice"));
+        assertTrue(messageBob.contains("Diane"));
+    }
+
+    @Test
+    void orderMessage() {
+        // Remove the start messages.
+        con1Alice.readSentMessage();
+        con2Bob.readSentMessage();
+
+        // We know the order of the tiles in the bag.
+        // And the game class will draw tiles in the order of the received player list.
+        // We can determine that this should be the resulting order of players.
+        assertEquals("order Alice Diane Clarice Bob", con1Alice.readSentMessage());
+
+        // Bob should have gotten the same message.
+        assertEquals("order Alice Diane Clarice Bob", con2Bob.readSentMessage());
+    }
+
+    @Test
+    void turnMessage() {
+        con3Clarice.purgeSentMessages();
+        con4Diane.purgeSentMessages();
+
+        game.doSingleGameThreadIteration();
+
+        String messageClarice = con3Clarice.readSentMessage();
+
+        System.out.println(messageClarice);
+
+        assertTrue(messageClarice.contains("tiles"));
+        assertTrue(messageClarice.contains("Alice"));
+        assertTrue(messageClarice.contains("Bob"));
+        assertTrue(messageClarice.contains("Clarice"));
+        assertTrue(messageClarice.contains("Diane"));
+        assertTrue(messageClarice.contains("turn"));
+
+        // Bob should have gotten the same message.
+        String messageDiane = con4Diane.readSentMessage();
+
+        System.out.println(messageDiane);
+
+        assertTrue(messageDiane.contains("tiles"));
+        assertTrue(messageDiane.contains("Alice"));
+        assertTrue(messageDiane.contains("Bob"));
+        assertTrue(messageDiane.contains("Clarice"));
+        assertTrue(messageDiane.contains("Diane"));
+        assertTrue(messageDiane.contains("turn"));
+    }
+
+    // TODO: Test more game stuff.
 }
