6f9fac285d4d290a1b94f684f87724f2ddf1a30f
Wybe Westra <wybe@ruurdwestra.nl>
2019-01-27 14:19:21 +0100

    + Added message sending to ServerPeer, and tests.

--- src/main/ss/spec/client/ServerPeer.java
+++ src/main/ss/spec/client/ServerPeer.java
@@ -1,5 +1,7 @@
 package ss.spec.client;
 
+import ss.spec.gamepieces.Move;
+import ss.spec.gamepieces.Tile;
 import ss.spec.networking.AbstractPeer;
 import ss.spec.networking.Connection;
 
@@ -14,9 +16,30 @@ public class ServerPeer extends AbstractPeer {
         System.out.println("Server says:" + message);
     }
 
+    // ---------------------------------------------------------------------------------------------
+
     public void sendConnectMessage(String name) {
         sendMessage("connect " + name);
 
         // TODO: Include list of extensions.
+    }
+
+    public void sendRequestMessage(int players) {
+        sendMessage("request " + players);
+    }
+
+
+    public void sendMoveMessage(Move move) {
+        sendMessage("place " +
+                move.getTile().encode() + " on " +
+                move.getIndex());
+    }
+
+    public void sendSkipMessage() {
+        sendMessage("skip");
+    }
+
+    public void sendExchangeMessage(Tile tile) {
+        sendMessage("exchange " + tile.encode());
     }
 }
--- src/test/ss/test/client/ServerPeerTest.java
+++ src/test/ss/test/client/ServerPeerTest.java
@@ -3,9 +3,13 @@ package ss.test.client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ss.spec.client.ServerPeer;
+import ss.spec.gamepieces.Color;
+import ss.spec.gamepieces.Move;
+import ss.spec.gamepieces.Tile;
 import ss.test.networking.MockConnection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ServerPeerTest {
 
@@ -25,5 +29,52 @@ class ServerPeerTest {
         peer.sendConnectMessage(name);
 
         assertEquals("connect " + name, connection.readSentMessage());
+    }
+
+    @Test
+    void sendRequestMessage() {
+        peer.sendRequestMessage(1);
+        assertEquals("request 1", connection.readSentMessage());
+
+        peer.sendRequestMessage(2);
+        assertEquals("request 2", connection.readSentMessage());
+
+        // Not a proper number of players, but the send message shouldn't check that anyway.
+        peer.sendRequestMessage(10);
+        assertEquals("request 10", connection.readSentMessage());
+    }
+
+    @Test
+    void sendMoveMessage() {
+        peer.sendMoveMessage(new Move(
+                new Tile(Color.GREEN, Color.BLUE, Color.PURPLE, 3), 4));
+        assertEquals("place GBP3 on 4", connection.readSentMessage());
+
+        peer.sendMoveMessage(new Move(
+                new Tile(Color.WHITE, Color.WHITE, Color.WHITE, 4), 4));
+        assertEquals("place WWW4 on 4", connection.readSentMessage());
+
+        // Can't go sending a null tile.
+        assertThrows(NullPointerException.class, () ->
+                peer.sendMoveMessage(new Move(null, 6)));
+    }
+
+    @Test
+    void sendSkipMessage() {
+        peer.sendSkipMessage();
+        assertEquals("skip", connection.readSentMessage());
+    }
+
+    @Test
+    void sendExchangeMessage() {
+        peer.sendExchangeMessage(new Tile(Color.GREEN, Color.YELLOW, Color.PURPLE, 9));
+        assertEquals("exchange GYP9", connection.readSentMessage());
+
+        peer.sendExchangeMessage(new Tile(Color.PURPLE, Color.WHITE, Color.RED, 1));
+        assertEquals("exchange PWR1", connection.readSentMessage());
+
+        // Can't go sending a null tile.
+        assertThrows(NullPointerException.class, () ->
+                peer.sendExchangeMessage(null));
     }
 }
