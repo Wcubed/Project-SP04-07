2a0319a66da982485679f16f3ca68a33cba484d4
Wybe Westra <wybe@ruurdwestra.nl>
2019-02-05 12:01:25 +0100

    + Added new todo's according to the requirements.

--- src/main/ss/spec/server/Server.java
+++ src/main/ss/spec/server/Server.java
@@ -9,7 +9,28 @@ import java.net.Socket;
 
 public class Server {
 
-    // TODO: Some classes in the server need JML. See module guide for which ones and how.
+    // TODO: Choose 3 classes where specifications are interesting and relevant: motivate why you
+    //  choose these (so not only in the server, but you can also choose from the client.
+    //  classes, and document the classes and all their methods with Javadoc, and with JML pre- and
+    //   post-
+    //  conditions and class invariants (JML specifications must type-check with OpenJML).
+
+    // TODO: Extensive documentation should be
+    //  provided for the three most complex self-defined classes in the server application.
+    //  as determined by the Weighted methods per class metric.
+
+    // TODO: There should be a README file with information about installation and starting the
+    //  game, indicating for
+    //  example which directories and files are necessary, and conditions for the installation.
+    //  After reading
+    //  this file, a user should be able to install and execute the game without any problem. The
+    //   README file
+    //   should be located in the root folder of the project.
+
+    // TODO: Compile the javadoc when ready.
+
+    // TODO: include the pre-compiled jar files with the packaged zip.
+
 
     // Protocol dictates port 4000.
     private static final int PORT = 4000;
